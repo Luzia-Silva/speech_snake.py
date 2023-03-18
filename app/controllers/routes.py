208d4df36b4dad6b02fef1bc12b5e25002ba0e3c
Luzia Gabriela Abreu da Silva Santos <73603007+Luzia-Silva@users.noreply.github.com>
2023-03-18 14:14:20 -0300

    create page audioupload

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -13,14 +13,11 @@ from app.models.analyzes import StartDetectionAndClickSynthesis
 def index():
     return render_template("index.html")
 
+@app.route("/audioupload")
+def audioupload():
+    return render_template("audioupload.html")
 
 @app.route("/analyzes")
 def graphics():
     images = StartDetectionAndClickSynthesis()
-    return render_template("analyzes.html", img_base64=images.image_base64)
-
-
-@app.route("/analyzes")
-def graphic03():
-    images = StartDetectionAndClickSynthesis()
-    return "teste"
+    return render_template("analyzes.html", img_base64=images.image_base64, dados=dados)

e7e2c7bf4dfc336f929f7e4faa26bd07aaf14c7a
Luzia Gabriela Abreu da Silva Santos <73603007+Luzia-Silva@users.noreply.github.com>
2023-04-15 17:09:30 -0300

    connect database mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= os.environ.get("SECRET_KEY")
-
+client = MongoClient(os.environ.get("DATABASE"))
+try:
+    client.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
 from app.controllers import routes
--- run.py
+++ run.py
@@ -1,18 +1,4 @@
 from app import app
-from pymongo.mongo_client import MongoClient
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
-    uri = os.environ.get("URI")
-    client = MongoClient(uri)
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        app.run()
-    except Exception as e:
-        print(e)
+    app.run()
